16a233cb0fdaa1f09cf816bfc2e7066f74de3ffa
micoFinlandes <setala3@hotmail.com>
2020-01-06 19:39:17 +0200

    TASK-2 template for multi platform client app.

---
+++ temperature/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from '@src/app/app-routing.module';
+import { AppComponent } from '@src/app/app.component';
+import { HomeComponent } from '@src/app/home/home.component';
+import { HighchartsChartComponent } from 'highcharts-angular';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    HighchartsChartComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ temperature/src/app/home/fruit.ts
@@ -0,0 +1,6 @@
+export class Fruit {
+    constructor(public type?: string, public count?: number, public price?: number) {
+    }
+}
+
+
